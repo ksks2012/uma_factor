7c4dbfe5bc045c4c95f60e75e87d7fff42fb69dc
ksks2012 <st890751999@gmail.com>
2022-11-13 19:53:27 +0800

    feat(total_factor): add total_factor for calculate total factor of a horse with two parents

--- test/testing_data_processor.py
+++ test/testing_data_processor.py
@@ -1,4 +1,5 @@
-from util.data_processor import sql_data_to_horse_info, check_parents
+from db_routine.sqlite import SqliteInstance
+from util.data_processor import sql_data_to_horse_info, check_parents, total_factor
 
 HORSE_INFO_IDX = [1, '低語溪流 - 超級小溪', None, None, None, '{"2": 1}', '{"5": 2}', '{}', '{"3": 1, "16": 1, "31": 2, "86": 1, "98": 2, "141": 1, "183": 1, "15": 1, "24": 3, "47": 2, "87": 3, "140": 2, "168": 2, "0": 2}']
 HORSE_INFO_IDX_RESULT = {'horse_id': 1, 'horse_name': '低語溪流 - 超級小溪', 'parent_one_id': None, 'parent_two_id': None, 'is_owner': None, 'blue_factor': '{"2": 1}', 'red_factor': '{"5": 2}', 'green_factor': '{}', 'white_factor': '{"3": 1, "16": 1, "31": 2, "86": 1, "98": 2, "141": 1, "183": 1, "15": 1, "24": 3, "47": 2, "87": 3, "140": 2, "168": 2, "0": 2}'}
@@ -14,6 +15,29 @@ def testing_check_parent():
     check_parents(0, 1, 2)
     check_parents(1, 1, 2)
 
+def test_total_factor():
+    sqlite_instance = SqliteInstance()
+    sqlite_instance.connect("var/data.db3")
+    horse_id = 0
+    horse_info = {}
+    parent_one_info = {}
+    parent_two_info = {}
+    try:
+        horse_info = sqlite_instance.search_horse(f"SELECT * FROM HorseData WHERE horse_id = {horse_id}")
+        horse_info = sql_data_to_horse_info(horse_info)[0]
+        print(horse_info)
+        parent_one_info = sqlite_instance.search_horse(f"SELECT * FROM HorseData WHERE horse_id = {int(horse_info['parent_one_id'])}")
+        parent_one_info = sql_data_to_horse_info(parent_one_info)[0]
+        print(parent_one_info)
+        parent_two_info = sqlite_instance.search_horse(f"SELECT * FROM HorseData WHERE horse_id = {int(horse_info['parent_two_id'])}")
+        parent_two_info = sql_data_to_horse_info(parent_two_info)[0]
+        print(parent_two_info)
+    except Exception as e:
+        print("test_total_factor fail %s" % e)
+    total_result = total_factor(horse_info, parent_one_info, parent_two_info)
+    print(total_result)
+
 if __name__ == '__main__':
     # testing_sql_data_to_horse_info()
-    testing_check_parent()
+    # testing_check_parent()
+    test_total_factor()  
--- util/data_processor.py
+++ util/data_processor.py
@@ -1,9 +1,10 @@
-from typing import List
+import json
+from typing import List, Mapping
 from db_routine.sqlite import SqliteInstance
 import util.path as PATH
 
 # sql data list to horse info data
-def sql_data_to_horse_info(data: List):
+def sql_data_to_horse_info(data: List) -> List:
     sqlite_instance = SqliteInstance()
     sqlite_instance.connect(PATH.DB_PATH)
     horse_data_field = sqlite_instance.select_field("HorseData")
@@ -44,3 +45,40 @@ def check_parents(parent_number: int, parent_id: int, child_id: int):
     
     sqlite_instance.set_parents(PARENT_NAME[parent_id], parent_id, child_id)
     print("check_parents: success")
+
+def _store_factor(total_factor: Mapping, horse_info: Mapping) -> Mapping:
+    """
+        total_factor
+            $store_key
+                $factor_name
+                    count
+                    stars
+    """
+
+    store_key = ['blue_factor', 'red_factor', 'green_factor', 'white_factor']
+
+    for key, value in horse_info.items():
+        if key in store_key:
+            if total_factor.get(key) is None:
+                total_factor[key] = {}
+            for factor_name, factor_stars in json.loads(value).items():
+                if factor_name in total_factor.get(key):
+                    total_factor[key][factor_name]['count'] += 1
+                    total_factor[key][factor_name]['stars'] += factor_stars
+                else:
+                    if total_factor[key].get(factor_name) is None:
+                        total_factor[key][factor_name] = {}
+                    total_factor[key][factor_name]['count'] = 1
+                    total_factor[key][factor_name]['stars'] = factor_stars
+
+    return total_factor
+
+def total_factor(horse_info: Mapping, parent_one_info: Mapping, parent_two_info: Mapping) -> Mapping:
+    # merge child and two parent
+    total_factor = {}
+
+    total_factor = _store_factor(total_factor, horse_info)
+    total_factor = _store_factor(total_factor, parent_one_info)
+    total_factor = _store_factor(total_factor, parent_two_info)
+
+    return total_factor
