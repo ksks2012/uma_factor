499bf7d16b361a31d2d17aefad58687c80c26d35
ksks2012 <st890751999@gmail.com>
2022-11-20 04:03:52 +0800

    feat(horse_data): add model horse_data

---
+++ model/horse_data.py
@@ -0,0 +1,24 @@
+from db_routine.sqlite import SqliteInstance
+from util.data_processor import sql_data_to_horse_info
+import util.path as PATH
+
+class Horse_DATA():
+    def __init__(self, child) -> None:
+        self.sqlite_instance = SqliteInstance()
+        self.sqlite_instance.connect(PATH.DB_PATH)
+
+        self.child_id = child
+        self.parent_one_id = -1
+        self.parent_two_id = -1
+        self.update_parent_id()
+
+    def update_parent_id(self):
+        child_info = self.sqlite_instance.select_horse_by_id(self.child_id)
+        self.child_info = sql_data_to_horse_info(child_info)[0]
+        self.parent_one_id = self.child_info.get("parent_one_id", -1)
+        self.parent_two_id = self.child_info.get("parent_two_id", -1)
+
+        parent_one_info = self.sqlite_instance.select_horse_by_id(self.parent_one_id)
+        self.parent_one_info = sql_data_to_horse_info(parent_one_info)[0]
+        parent_two_info = self.sqlite_instance.select_horse_by_id(self.parent_two_id)
+        self.parent_two_info = sql_data_to_horse_info(parent_two_info)[0]
---
+++ test/testing_model.py
@@ -0,0 +1,13 @@
+from model.horse_data import Horse_DATA
+
+def testing_horse_data():
+    left_horse_data = Horse_DATA(0)
+    right_horse_data = Horse_DATA(3)
+
+    print(left_horse_data.child_id, left_horse_data.parent_one_id, left_horse_data.parent_two_id)
+    print(left_horse_data.child_info)
+    print(left_horse_data.parent_one_info)
+    print(left_horse_data.parent_two_info)
+
+if __name__ == '__main__':
+    testing_horse_data()
