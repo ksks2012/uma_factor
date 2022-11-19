120c3e5071b68a2300e8fd7184bfa0a565059edc
ksks2012 <st890751999@gmail.com>
2022-11-20 03:46:06 +0800

    refactor(parent_factor_info_layout): change pair_info_layout to parent_factor_info_layout

--- test/testing_parent_factor_info_layout.py
+++ test/testing_parent_factor_info_layout.py
@@ -1,11 +1,11 @@
 from kivy.base import runTouchApp
 from testing_data_processor import test_pair_factor
-from view.pair_info_layout import PairInfoLayout
+from view.sub_view.parent_factor_info_layout import ParentFactorInfoLayout
 
 def testing_pair_info_layout():
     pair_info = test_pair_factor()
     # create
-    runTouchApp(PairInfoLayout(horse_info=pair_info))
+    runTouchApp(ParentFactorInfoLayout(horse_info=pair_info))
 
 if __name__ == '__main__':
     testing_pair_info_layout()
